19eaa3b0c9294886ec65f5da39ebddfcc8add66e
SkuldNorniern <skuldnorniern@gmail.com>
2026-07-12 19:00:53 +0900

    fix: adapt neoforge tag providers to MC 26.2 TagsProvider API

--- fabric/src/main/java/toughasnails/fabric/mixin/client/MixinGui.java
+++ fabric/src/main/java/toughasnails/fabric/mixin/client/MixinGui.java
@@ -4,15 +4,15 @@
  ******************************************************************************/
 package toughasnails.fabric.mixin.client;
 
-import net.minecraft.client.gui.Gui;
 import net.minecraft.client.gui.GuiGraphicsExtractor;
+import net.minecraft.client.gui.Hud;
 import org.spongepowered.asm.mixin.Mixin;
 import org.spongepowered.asm.mixin.injection.At;
 import org.spongepowered.asm.mixin.injection.Inject;
 import org.spongepowered.asm.mixin.injection.callback.CallbackInfo;
 import toughasnails.temperature.TemperatureHooksClient;
 
-@Mixin(Gui.class)
+@Mixin(Hud.class)
 public class MixinGui
 {
     @Inject(method="extractSelectedItemName", at=@At(value="HEAD"))
--- neoforge/src/main/java/toughasnails/neoforge/datagen/provider/TANBlockTagsProvider.java
+++ neoforge/src/main/java/toughasnails/neoforge/datagen/provider/TANBlockTagsProvider.java
@@ -6,7 +6,9 @@ package toughasnails.neoforge.datagen.provider;
 
 import net.minecraft.core.HolderLookup;
 import net.minecraft.data.PackOutput;
+import net.minecraft.resources.ResourceKey;
 import net.minecraft.tags.BlockTags;
+import net.minecraft.world.level.block.Block;
 import net.minecraft.world.level.block.Blocks;
 import net.neoforged.neoforge.common.data.BlockTagsProvider;
 import toughasnails.api.block.TANBlocks;
@@ -14,6 +16,7 @@ import toughasnails.core.ToughAsNails;
 import toughasnails.init.ModTags;
 
 import java.util.concurrent.CompletableFuture;
+import java.util.stream.Stream;
 
 public class TANBlockTagsProvider extends BlockTagsProvider
 {
@@ -22,16 +25,21 @@ public class TANBlockTagsProvider extends BlockTagsProvider
         super(output, lookupProvider, ToughAsNails.MOD_ID);
     }
 
+    private static ResourceKey<Block> key(Block block)
+    {
+        return block.builtInRegistryHolder().key();
+    }
+
     @Override
     protected void addTags(HolderLookup.Provider provider)
     {
         // Vanilla tags
-        this.tag(BlockTags.MINEABLE_WITH_AXE).add(TANBlocks.RAIN_COLLECTOR, TANBlocks.WATER_PURIFIER);
-        this.tag(BlockTags.MINEABLE_WITH_PICKAXE).add(TANBlocks.THERMOREGULATOR, TANBlocks.TEMPERATURE_GAUGE);
+        this.tag(BlockTags.MINEABLE_WITH_AXE).addAll(Stream.of(TANBlocks.RAIN_COLLECTOR, TANBlocks.WATER_PURIFIER).map(TANBlockTagsProvider::key));
+        this.tag(BlockTags.MINEABLE_WITH_PICKAXE).addAll(Stream.of(TANBlocks.THERMOREGULATOR, TANBlocks.TEMPERATURE_GAUGE).map(TANBlockTagsProvider::key));
 
         // TAN tags
-        this.tag(ModTags.Blocks.COOLING_BLOCKS).add(Blocks.SOUL_FIRE, Blocks.SOUL_CAMPFIRE, Blocks.SOUL_LANTERN, Blocks.PACKED_ICE, Blocks.BLUE_ICE, Blocks.POWDER_SNOW_CAULDRON);
-        this.tag(ModTags.Blocks.HEATING_BLOCKS).add(Blocks.FIRE, Blocks.CAMPFIRE, Blocks.LANTERN, Blocks.LAVA, Blocks.MAGMA_BLOCK, Blocks.LAVA_CAULDRON);
-        this.tag(ModTags.Blocks.PASSABLE_BLOCKS).addTags(BlockTags.DOORS, BlockTags.TRAPDOORS).add(Blocks.SCAFFOLDING);
+        this.tag(ModTags.Blocks.COOLING_BLOCKS).addAll(Stream.of(Blocks.SOUL_FIRE, Blocks.SOUL_CAMPFIRE, Blocks.SOUL_LANTERN, Blocks.PACKED_ICE, Blocks.BLUE_ICE, Blocks.POWDER_SNOW_CAULDRON).map(TANBlockTagsProvider::key));
+        this.tag(ModTags.Blocks.HEATING_BLOCKS).addAll(Stream.of(Blocks.FIRE, Blocks.CAMPFIRE, Blocks.LANTERN, Blocks.LAVA, Blocks.MAGMA_BLOCK, Blocks.LAVA_CAULDRON).map(TANBlockTagsProvider::key));
+        this.tag(ModTags.Blocks.PASSABLE_BLOCKS).addTags(BlockTags.DOORS, BlockTags.TRAPDOORS).add(key(Blocks.SCAFFOLDING));
     }
 }
